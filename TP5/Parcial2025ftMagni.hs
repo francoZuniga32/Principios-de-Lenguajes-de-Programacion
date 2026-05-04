0176334c6f78b45babfc3b2844fbbc242c3ac26d
francozuniga32 <franco.ojeda@est.fi.uncoma.edu.ar>
2026-05-04 18:50:21 -0300

    feat: correccion sobre ejercicio parcial

--- TP5/Parcial2025ftMagni.hs
+++ TP5/Parcial2025ftMagni.hs
@@ -1,5 +1,5 @@
 operacion :: Double -> Double -> Double
-operacion x c = ( x^3 - 5*c) / ( x*2 +c-1)
+operacion x c = ( (x^3) + (5*c)) / ( (x*2) + ( c-1))
 
 sumatoria :: Int -> Int -> Int -> Double
 sumatoria x k c 
@@ -7,12 +7,12 @@ sumatoria x k c
     | x == k = op
     | otherwise = 0
     where
-    op = operacion (fromIntegral x) (fromIntegral c)
+        op = operacion (fromIntegral x) (fromIntegral c)
 
 principal :: (Int, Int) -> Int -> String
 principal (x, k) c
-    | x < k = show(x)++" "++show(sumatoria x k c)++"\n"++ principal( ( x+1 ), k ) c
-    | x == k = show(x)++" "++show(sumatoria x k c)
+    | x < k = show(x)++" "++show((sumatoria 1 x c))++"\n"++ ( principal( ( x+1 ), k ) c )
+    | x == k = show(x)++" "++show((sumatoria 1 x c))
     | otherwise = "Err"
     
 main :: IO()
