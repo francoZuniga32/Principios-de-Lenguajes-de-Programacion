3ff3550dcc204730358b4050e355f234b796e6e6
franco zuñiga <54842987+francoZuniga32@users.noreply.github.com>
2026-06-29 18:28:31 -0300

    filtrar umbral

--- TP8/LTAEC.pl
+++ TP8/LTAEC.pl
@@ -9,3 +9,10 @@ unir(L1,[],L1).
 unir(L1, [X|L2], [X|R]) :- unir(L1, L2, R).
 
 rotar(L1,N,R) :- fragmentar(L1,0,N,R1,R2), unir(R1,R2,R).
+
+
+%filtrar umbral%
+
+mayores([],_,[],0).
+mayores([X1|L1], U,[X1|R], C2) :- X1 @>= U,mayores(L1, U, R, C), C2 is C + 1.
+mayores([_|L1], U, R, C) :- mayores(L1, U, R, C).
