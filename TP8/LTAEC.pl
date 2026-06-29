d4f73e7295abc8a5eb4560dbdfd45c6f9ea3f08c
franco zuñiga <54842987+francoZuniga32@users.noreply.github.com>
2026-06-29 18:02:16 -0300

    LTAEC

---
+++ TP8/LTAEC.pl
@@ -0,0 +1,10 @@
+%La Tenes Adentro Erick%
+
+%rotar n elementos%
+fragmentar(L1, N, N,[],L1). 
+fragmentar([X|L1],I, N, [X|R], R2) :- I @< N, I2 is I + 1, fragmentar(L1, I2, N, R, R2). 
+
+unir(L1,[],L1).
+unir(L1, [X|L2], [X|R]) :- unir(L1, L2, R).
+
+rotar(L1,N,R) :- fragmentar(L1,0,N,R1,R2), unir(R1,R2,R).
